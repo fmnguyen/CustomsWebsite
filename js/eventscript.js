0ca701f46d742407fc8094e281317ba7a44d00af
fmnguyen <fmnguyen@uw.edu>
2015-10-06 13:18:13 -0700

    Added responsive carousel

--- js/eventscript.js
+++ js/eventscript.js
@@ -1,23 +1,23 @@
 $(document).ready(function(){
 	$('.events-container').slick({
 		infinite: false,
-		slidesToShow: 4,
-  		slidesToScroll: 4,
   		focusOnSelect: false,
-  		appendArrows: $('.arrows-container'),
+  		arrows: false,
+  		slidesToShow: 4,
+		slidesToScroll: 4,
   		responsive: [
 		    {
 		      breakpoint: 1024,
 		      settings: {
-		        slidesToShow: 4,
-		        slidesToScroll: 4,
+		        slidesToShow: 3,
+		        slidesToScroll: 3,
 		      }
 		    },
 		    {
 		      breakpoint: 768,
 		      settings: {
-		        slidesToShow: 3,
-		        slidesToScroll: 3
+		        slidesToShow: 2,
+		        slidesToScroll: 2
 		      }
 		    },
 		    {
@@ -27,9 +27,12 @@ $(document).ready(function(){
 		        slidesToScroll: 2
 		      }
 		    }
-		    // You can unslick at a given breakpoint now by adding:
-		    // settings: "unslick"
-		    // instead of a settings object
-		  ]
+		  ],
+	});
+	$('.arrow-left').click(function(e){
+		$('.events-container').slick('slickPrev');
+	});
+	$('.arrow-right').click(function(e){
+		$('.events-container').slick('slickNext');
 	});
 });
