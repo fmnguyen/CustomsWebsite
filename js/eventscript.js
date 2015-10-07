7db1ae290029af2cde9968526ac36d8cf7aa6199
fmnguyen <fmnguyen@uw.edu>
2015-10-07 13:31:58 -0700

    Events Carousel  now reading directly from events.json

--- js/eventscript.js
+++ js/eventscript.js
@@ -1,40 +1,57 @@
 $(document).ready(function(){
 
+	$.getJSON('events.json', function( data ) {
+		$.each(data.events.event, function(i, item) {
+			var $month = $('<span>', {class: "event-date-month", text: item.date.month.substring(0,3)}),
+				$day = $('<span>', {class: "event-date-day", text: item.date.day}),
+				$imgwrp = $('<div>', {class: "img-wrapper"}),
+				$img = $('<img>', {src: item.img, class: "event-image"}),
+				$date = $('<div>', {class: "event-date"}).append($month, "</br>", $day);
+				$summary = $('<div>', {class: "event-summary"}),
+				$title = $('<h2>', {class: "event-title", text: item.title}),
+				$ticket = $('<a>', {class: "event-ticket", text: "Tickets", href: item.ticket.url}),
+				$show = $('<div>', {id: item.id, class: "event-show"});
+				$summary.append($title, $ticket);
+				$imgwrp.append($img, $date);
+				$show.append($imgwrp, $summary);
+				$('.events-container').append($show);
+		});
 
-	$('.events-container').slick({
-		infinite: false,
-  		focusOnSelect: false,
-  		arrows: false,
-  		slidesToShow: 4,
-		slidesToScroll: 4,
-  		responsive: [
-		    {
-		      breakpoint: 1024,
-		      settings: {
-		        slidesToShow: 3,
-		        slidesToScroll: 3,
-		      }
-		    },
-		    {
-		      breakpoint: 768,
-		      settings: {
-		        slidesToShow: 2,
-		        slidesToScroll: 2
-		      }
-		    },
-		    {
-		      breakpoint: 480,
-		      settings: {
-		        slidesToShow: 2,
-		        slidesToScroll: 2
-		      }
-		    }
-		  ],
-	});
-	$('.arrow-left').click(function(e){
-		$('.events-container').slick('slickPrev');
-	});
-	$('.arrow-right').click(function(e){
-		$('.events-container').slick('slickNext');
+		$('.events-container').slick({
+			infinite: false,
+	  		focusOnSelect: false,
+	  		arrows: false,
+	  		slidesToShow: 4,
+			slidesToScroll: 4,
+	  		responsive: [
+			    {
+			      breakpoint: 1024,
+			      settings: {
+			        slidesToShow: 3,
+			        slidesToScroll: 3,
+			      }
+			    },
+			    {
+			      breakpoint: 768,
+			      settings: {
+			        slidesToShow: 2,
+			        slidesToScroll: 2
+			      }
+			    },
+			    {
+			      breakpoint: 480,
+			      settings: {
+			        slidesToShow: 2,
+			        slidesToScroll: 2
+			      }
+			    }
+			  ],
+		});
+		$('.arrow-left').click(function(e){
+			$('.events-container').slick('slickPrev');
+		});
+		$('.arrow-right').click(function(e){
+			$('.events-container').slick('slickNext');
+		});
 	});
 });
